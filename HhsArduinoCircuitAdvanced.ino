ee78a72ddb38328d5b1b7953bc5aaccd46f2ff34
timvisee <timvisee@gmail.com>
2016-01-08 11:36:45 +0100

    Added docs to main file

--- HhsArduinoCircuitAdvanced.ino
+++ HhsArduinoCircuitAdvanced.ino
@@ -1,9 +1,29 @@
+/**
+ * HhsArduinoCircuitAdvanced.
+ * An Arduino challenge for HHS by Tim Visee.
+ *
+ *
+ * This project requires some external libraries to be installed, in the Arduino libraries directory.
+ * - Arduino:
+ *   The default Arduino libraries, installed with the Arduino installer.
+ *
+ *
+ * @author Tim Visee
+ * @website http://timvisee.com/
+ */
+
 #include <Arduino.h>
 
+/**
+ * Called one on set up.
+ */
 void setup() {
 
 }
 
+/**
+ * Called each loop.
+ */
 void loop() {
 
 }
