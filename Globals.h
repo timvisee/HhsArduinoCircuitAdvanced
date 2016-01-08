fe37357fc5f2f1316dcaf4b95da3ad567b90c792
timvisee <timvisee@gmail.com>
2016-01-08 11:34:30 +0100

    Included Globals.h settings file

---
+++ Globals.h
@@ -0,0 +1,25 @@
+/**
+ * NumberGuessGame.
+ * An Arduino challenge for HHS by Tim Visee.
+ *
+ * @author Tim Visee
+ * @website http://timvisee.com/
+ */
+
+#ifndef NUMBERGUESSGAME_GLOBALS_H
+#define NUMBERGUESSGAME_GLOBALS_H
+
+#include <Arduino.h>
+
+/**
+ * Debug mode.
+ * True to enable, false to disable.
+ */
+const bool DEBUG = true;
+
+/**
+ * The USB serial baud rate.
+ */
+const int SERIAL_USB_BAUD = 19200;
+
+#endif //NUMBERGUESSGAME_GLOBALS_H
