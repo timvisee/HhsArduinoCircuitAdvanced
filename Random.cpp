203ccd0c277e72b887b9c9162f83967e10d15d2b
Tim Visée <timvisee@gmail.com>
2016-01-19 00:16:53 +0100

    Created Random class

---
+++ Random.cpp
@@ -0,0 +1,34 @@
+/**
+ * HhsArduinoCircuitAdvanced.
+ * An Arduino challenge for HHS by Tim Visee.
+ *
+ * @author Tim Visee
+ * @website http://timvisee.com/
+ */
+
+#include "Random.h"
+
+int Random::nextInt(int max) {
+    return nextInt(0, max);
+}
+
+int Random::nextInt(int min, int max) {
+    return (int) random((long) min, (long) max);
+}
+
+void Random::randomize() {
+    // Define the new seed variable
+    unsigned int seed = 1;
+
+    // Create a semi-random number to use as seed
+    for(int i = 0; i < 6; i++)
+        seed *= analogRead((uint8_t) i);
+
+    // Set the actual seed
+    setSeed((unsigned int) analogRead(0));
+}
+
+void Random::setSeed(unsigned int seed) {
+    // Set the seed in the Arduino library
+    randomSeed(seed);
+}
