543945109f21e2e089c369e4a8bd95599f9125b3
Tim Visée <timvisee@gmail.com>
2016-01-19 00:04:09 +0100

    Created ButtonManager class

---
+++ ButtonManager.h
@@ -0,0 +1,27 @@
+/**
+ * HhsArduinoCircuitAdvanced.
+ * An Arduino challenge for HHS by Tim Visee.
+ *
+ * @author Tim Visee
+ * @website http://timvisee.com/
+ */
+
+#ifndef NUMBERGUESSGAME_BUTTONMANAGER_H
+#define NUMBERGUESSGAME_BUTTONMANAGER_H
+
+#include <Arduino.h>
+#include "Globals.h"
+#include "Button.h"
+
+/**
+ * Button manager class.
+ */
+class ButtonManager {
+public:
+    /**
+     * Main button instance.
+     */
+    static Button button;
+};
+
+#endif //NUMBERGUESSGAME_BUTTONMANAGER_H
