e56dce26462e8c952cffd0bd82e1053f6d6dad3a
Tim Visée <timvisee@gmail.com>
2016-01-19 00:31:12 +0100

    Created Timer class

---
+++ Timer.cpp
@@ -0,0 +1,65 @@
+/**
+ * HhsArduinoCircuitAdvanced.
+ * An Arduino challenge for HHS by Tim Visee.
+ *
+ * @author Tim Visee
+ * @website http://timvisee.com/
+ */
+
+#include "Timer.h"
+
+Timer::Timer() {
+    // Set the timeout to it's default
+    this->timeout = -1;
+
+    // Set the timeout variable
+    this->timeoutDelay = 1000;
+}
+
+Timer::Timer(int timeout) {
+    // Set the timeout to it's default
+    this->timeout = -1;
+
+    // Set the timeout variable
+    this->timeoutDelay = timeout;
+}
+
+Timer::Timer(int timeout, bool start) {
+    // Set the timeout to it's default
+    this->timeout = -1;
+
+    // Set the timeout variable
+    this->timeoutDelay = timeout;
+
+    // Start the timer
+    if(start)
+        this->start();
+}
+
+int Timer::getTimeoutDelay() {
+    return this->timeoutDelay;
+}
+
+void Timer::setTimeoutDelay(int timeout) {
+    this->timeoutDelay = timeout;
+}
+
+void Timer::start() {
+    this->start(this->getTimeoutDelay());
+}
+
+void Timer::start(int timeoutDelay) {
+    // Set the timeout
+    this->timeout = millis() + timeoutDelay;
+
+    // Update the timeout delay
+    this->timeoutDelay = timeoutDelay;
+}
+
+bool Timer::isFinished() {
+    return this->timeout < millis() && this->timeout >= 0;
+}
+
+void Timer::stop() {
+    this->timeout = -1;
+}
---
+++ Timer.h
@@ -0,0 +1,88 @@
+/**
+ * HhsArduinoCircuitAdvanced.
+ * An Arduino challenge for HHS by Tim Visee.
+ *
+ * @author Tim Visee
+ * @website http://timvisee.com/
+ */
+
+#ifndef NUMBERGUESSGAME_TIMER_H
+#define NUMBERGUESSGAME_TIMER_H
+
+#include <Arduino.h>
+
+/**
+ * Timer class.
+ */
+class Timer {
+private:
+    /**
+     * Timeout delay in milliseconds.
+     * */
+    int timeoutDelay;
+
+    /**
+     * Timeout as time in milliseconds, represents millis() value.
+     * */
+    long timeout;
+
+public:
+    /**
+     * Constructor.
+     */
+    Timer();
+
+    /**
+     * Constructor.
+     *
+     * @param timeout Timeout in milliseconds.
+     */
+    Timer(int timeout);
+
+    /**
+     * Constructor.
+     *
+     * @param timeout Timeout in milliseconds.
+     * @param start True to immediately start the timer, false if not.
+     */
+    Timer(int timeout, bool start);
+
+    /**
+     * Get the timeout delay in milliseconds.
+     *
+     * @return Timeout delay.
+     */
+    int getTimeoutDelay();
+
+    /**
+     * Set the timeout delay in milliseconds.
+     *
+     * @return Timeout delay.
+     */
+    void setTimeoutDelay(int timeout);
+
+    /**
+     * Start the timer.
+     */
+    void start();
+
+    /**
+     * Start the timer with the specified delay in milliseconds.
+     * This also updates the default delay of this timer.
+     */
+    void start(int timeoutDelay);
+
+    /**
+     * Check whether the timer is finished.
+     *
+     * @return True if finished, false if not.
+     */
+    bool isFinished();
+
+    /**
+     * Stop the timer if it's currently active.
+     */
+    void stop();
+};
+
+#endif //NUMBERGUESSGAME_TIMER_H
